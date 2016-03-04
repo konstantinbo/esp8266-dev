aa3dc1ee6e454579e3c2ee0340492d55bb6d42b5
konsi1996 <s4234762@mail.zih.tu-dresden.de>
2016-03-04 21:26:18 +0100

    Added custom LED to D1 pin

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,13 +1,16 @@
 const short int BUILTIN_LED1 = 2; //GPIO2
 const short int BUILTIN_LED2 = 16;//GPIO16
+const short int custom_led = 5; //D1 output
 
 void setup() {
-  pinMode(BUILTIN_LED1, OUTPUT); // Initialize the BUILTIN_LED1 pin as an output 
-  pinMode(BUILTIN_LED2, OUTPUT); // Initialize the BUILTIN_LED2 pin as an output
+  pinMode(BUILTIN_LED1, OUTPUT); // Initialize the BUILTIN_LED1 pin as an output (RED LED) 
+  pinMode(BUILTIN_LED2, OUTPUT); // Initialize the BUILTIN_LED2 pin as an output (Blue LED)
+  pinMode(custom_led, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(BUILTIN_LED1, LOW); // Turn the LED ON by making the voltage LOW
+  digitalWrite(custom_led, HIGH);
+  digitalWrite(BUILTIN_LED1, LOW); // Turn the LED ON by making the voltage LOW 
   digitalWrite(BUILTIN_LED2, HIGH); // Turn the LED off by making the voltage HIGH
   delay(1000); // Wait for a second 
   digitalWrite(BUILTIN_LED1, HIGH); // Turn the LED off by making the voltage HIGH 
